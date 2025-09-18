b30b612aceed24b545bc83617906d2488d6a507e
Matheus <matheuscostajesus@uni9.edu.br>
2025-09-18 17:08:58 -0300

    :tada:

---
+++ linguagemjava/ExercicioDaAtividade1.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = 10;
+	    double b = 20.5;
+	    boolean c = true;
+	    
+	    double soma = a + b;
+	    System.out.println("Soma: " + soma);
+	    
+	    System.out.println(!c);
+	    
+		
+	}
+}
